1b7cbd81be82d7c8042e6fd8c369b6ee705453c4
ichiroprogrammer <160194260+ichiroprogrammer@users.noreply.github.com>
2024-10-16 17:24:29 +0900

    add exp term

---
+++ example/term_explanation/template_ut.cpp
@@ -0,0 +1,54 @@
+#include <vector>
+
+#include "gtest_wrapper.h"
+
+#include "nstd_type2str.h"
+#include "suppress_warning.h"
+
+namespace {
+
+TEST(TermExp, template_constant)
+{
+    // @@@ sample begin 0:0
+
+    auto a = std::vector<int>{1, 2, 3};
+
+    static_assert(std::is_same_v<decltype(a), std::vector<int>>);
+    // @@@ sample end
+}
+
+TEST(TermExp, template_constant_new)
+{
+    // clang-format off
+    // @@@ sample begin 0:1
+
+    auto a = std::vector{1, 2, 3};
+
+    static_assert(std::is_same_v<decltype(a), std::vector<int>>);  // テンプレート引数がintと推論
+    // @@@ sample end
+    // clang-format on
+}
+}  // namespace
+// @@@ sample begin 1:0
+
+template <typename T>
+struct is_void {
+    static constexpr bool value = false;
+};
+
+template <>
+struct is_void<void> {
+    static constexpr bool value = true;
+};
+
+static_assert(is_void<void>::value);
+static_assert(!is_void<int>::value);
+// 以上はC++14以前のスタイル
+
+// 以下はC++17から導入された
+template <typename T>
+constexpr bool is_void_v = is_void<T>::value;
+
+static_assert(is_void_v<void>);
+static_assert(!is_void_v<int>);
+// @@@ sample end
