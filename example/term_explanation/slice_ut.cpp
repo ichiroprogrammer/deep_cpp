3155b0ad6a37409c2e85362d987a97c7fa12ed17
ichiroprogrammer <160194260+ichiroprogrammer@users.noreply.github.com>
2025-09-25 21:11:55 +0900

    update slicing

--- example/term_explanation/slice_ut.cpp
+++ example/term_explanation/slice_ut.cpp
@@ -54,15 +54,15 @@ TEST(Slicing, reference)
 
     d2_ref = d1;                     // d2_refはBase&型で、d2へのリファレンス
     ASSERT_STREQ("d1", d2.Name0());  // OK
-    /*  本来なら↓が成立してほしいが...
+    /*  本来なら↓が成立してほしいが.、、、
     ASSERT_STREQ("d1", d2.Name1()); */
     ASSERT_STREQ("d0", d2.Name1());  // スライシングの影響でDerived::name1_はコピーされない
     // @@@ sample end
 }
-// @@@ sample begin 1:0
-
 TEST(Slicing, array)
 {
+    // @@@ sample begin 1:0
+
     Derived d_array[]{{"0", "1"}, {"2", "3"}};
     Base*   b_ptr = d_array;  // この代入までは問題ないが、b_ptr[1]でのアクセスで問題が起こる
 
@@ -70,11 +70,10 @@ TEST(Slicing, array)
     ASSERT_STREQ("0", b_ptr[0].Name0());    // OK
 
     ASSERT_STREQ("2", d_array[1].Name0());  // OK
-#if 0  // スライシングに類似した問題で、以下のテストは失敗する。
-    ASSERT_STREQ("2", b_ptr[1].Name0());  // NG
-#else  // こうすればテストは通るが、、、
-    ASSERT_STREQ("1", b_ptr[1].Name0());  // NG
-#endif
+    /* スライシングに類似した問題で、以下のテストは失敗する。
+    ASSERT_STREQ("2", b_ptr[1].Name0());    NG */
+    // こうすればテストは通るが、、、
+    ASSERT_STREQ("1", b_ptr[1].Name0());  // NG 
+    // @@@ sample end
 }
-// @@@ sample end
 }  // namespace
